ebf664dbd7070d41892224a9834c6b90eb5e5a39
MZA <last.randomness@gmail.com>
2019-02-02 12:06:24 -0800

    Initial Commit

---
+++ 08 - iOS 11 Development Essential Training Working with Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  08 - iOS 11 Development Essential Training Working with Views
+//
+//  Created by TikTak on 2/2/19.
+//  Copyright © 2019 TikTak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
